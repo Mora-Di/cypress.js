d9acc81ca6b1e331dc2265620d02457cd418b1ff
Diana <achibaevadi@gmail.com>
2025-08-21 12:45:42 +0300

    Update my_buy_new_avatar.cy.js

--- e2e/my_buy_new_avatar.cy.js
+++ e2e/my_buy_new_avatar.cy.js
@@ -3,19 +3,16 @@ describe('Проверка покупки нового аватара', function
          cy.visit('https://pokemonbattle.ru/');                          // переходим на сайт
         
          cy.get('.style_1_popup_white_title').contains('Битва покемонов'); // сравнение с текстом
-         cy.get('[href="https://id.vk.com/auth?uuid=GBFBV2654u2jhtrRTJS45jw6435454yyt3y4d2354&app_id=51935122&response_type=silent_token&redirect_uri=https://api.pokemonbattle.ru/v2/technical_routes/callback_vk"] > .auth_content_social_icon').should('be.visible'); // кнопка"ВК" виден пользователю
          cy.get('.footer_studio').should('be.visible');                    // лого компании видно пользователю
 
          cy.get('input[id="k_email"]').type('USER_LOGIN');          // вводим логин
          cy.get('input[id="k_password"]').type('USER_PASSWORD');    // вводим пароль
          cy.get('button[type="submit"]').click();                   // нажимаем кнопку Подтвердить
 
-         cy.wait(500);
          cy.get('.style_1_heading_38_400_pokemon_classic').contains('Покемоны'); // сравнение с текстом
          cy.get('.header_interactive_button_link_to_pokemons > .interactive_button > .wrapper_img > .interactive_button_img').should('be.visible');  // иконка Покебола видна пользователю
-         cy.get('.header_card_trainer').click();                                 // Клик в шапке на аву тренера
-         cy.wait(500);
-
+         cy.get('.header_card_trainer').click();
+        
          cy.get('.k_mobile > :nth-child(5) > #dropdown > img').click(); // нажимаем кнопку Смена аватара
          cy.get('.pokemon__title').contains('Магазин');                 // сравнение с текстом
          cy.get('.available > button').first().click();                 // кликаем Купить у первого доступного аватара
@@ -24,7 +21,6 @@ describe('Проверка покупки нового аватара', function
          cy.get('.card_csv').type('125');                             // вводим CVV карты
          cy.get('.card_date').type('1226');                           // вводим срок действия карты
          cy.get('.card_name').type('NAME');                           // вводим имя владельца карты
-         cy.wait(500);
          
          cy.get('.style_1_base_button_payment_body button.style_1_base_button_payment:not(.disable)').click();    // нажимаем кнопку Оплатить
          
